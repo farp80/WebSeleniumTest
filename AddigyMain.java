9edbe6b935a4d353bfc468d3def6a82af2e2ebf5
Fidel Rosell <farp80@gmail.com>
2020-01-14 21:07:17 -0500

    added java file

---
+++ AddigyMain.java
@@ -0,0 +1,15 @@
+package addigy.fidel.selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class AddigyMain {
+
+	WebDriver webDriver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
